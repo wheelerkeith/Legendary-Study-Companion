130d212d0ea23f41608fc49e774ea10f38383749
Chris Charnecki <christopher.charnecki@gmail.com>
2020-03-13 13:52:38 -0500

    installed bootstrap, jquery; added search component

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  searchQuery: String = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  search() {
+    console.log("searching");
+  }
+
+}
