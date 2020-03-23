0ad0d39ede49a6e45ec6676b2fa448f7fd0dd35e
Chris Charnecki <christopher.charnecki@gmail.com>
2020-03-23 00:19:59 -0500

    adding saving resources

--- src/app/services/resource.service.ts
+++ src/app/services/resource.service.ts
@@ -14,8 +14,9 @@ export const httpOptions = {
 export interface Resource {
   title: String,
   url: String,
-  likeCount: Number,
-  source: String
+  likeCount: number,
+  source: String,
+  saved: Boolean
 }
 
 @Injectable({
@@ -23,7 +24,8 @@ export interface Resource {
 })
 export class ResourceService {
 
-  private urlEndpoint: String = "http://ec2-3-21-237-82.us-east-2.compute.amazonaws.com:8090/LegendaryStudyCompanionBackend-0.0.1-SNAPSHOT/";
+  private awsUrlEndpoint: String = "http://ec2-3-21-237-82.us-east-2.compute.amazonaws.com:8090/LegendaryStudyCompanionBackend-0.0.1-SNAPSHOT/";
+  private urlEndpoint: String = "http://localhost:8080";
 
   constructor(private http: HttpClient) { }
 
@@ -33,11 +35,11 @@ export class ResourceService {
     return this.http.get<Resource[]>(resourceQueryUrl, httpOptions);
   }
 
-  postSavedResource(resource: Resource) {
+  postSavedResource(resource: Resource): Observable<Resource> {
     let tokenArray = sessionStorage.token.split(':',2);
     httpOptions.headers = httpOptions.headers.set('Authorization', `${sessionStorage.token}`);
-    let resourceQueryUrl = this.urlEndpoint + `user/id/resources`;
-    return this.http.post<any>(resourceQueryUrl, httpOptions);
+    let resourceQueryUrl = this.urlEndpoint + `user/${tokenArray[0]}/resources`;
+    return this.http.post<any>(resourceQueryUrl, resource, httpOptions);
   }
 
   postFlaggedResource(resource: Resource) {
